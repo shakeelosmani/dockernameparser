ff7820192fabc73dd0044c04efe126256572885e
Shakeel Osmani <shakeel.osmani@gmail.com>
2020-04-10 19:12:43 -0400

    add try catch in case

--- flask/app/endpoint.py
+++ flask/app/endpoint.py
@@ -5,5 +5,8 @@ from nameparser import HumanName
 
 @app.route("/parse/<name>")
 def index(name):
-  return_name = HumanName(name)
-  return json.dumps(return_name.as_dict())
+  try:
+    return_name = HumanName(name)
+    return json.dumps(return_name.as_dict())
+  except Exception as e:
+    return json.dumps(str(e))
